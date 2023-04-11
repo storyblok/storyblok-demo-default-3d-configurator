ed0bc46127be770d760778fa3c07c13cada0ebdb
alvarosabu <alvaro.saburido@gmail.com>
2023-04-11 11:46:09 +0200

    feat: loading screen

--- components/TheExperience.client.vue
+++ components/TheExperience.client.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { BasicShadowMap, sRGBEncoding, NoToneMapping, Color, MeshStandardMaterial } from 'three'
+import { BasicShadowMap, sRGBEncoding, NoToneMapping, Color, MeshStandardMaterial, DefaultLoadingManager } from 'three'
 import { TresCanvas, useTres } from '@tresjs/core'
 import { OrbitControls, Environment, GLTFModel, useTweakPane } from '@tresjs/cientos'
 
@@ -26,9 +26,37 @@ watchEffect(() => {
     console.log('context', context)
   }
 })
+
+const hasFinishLoading = ref(false)
+const progress = ref(0)
+
+let saveLastTotalLoaded = 0
+
+DefaultLoadingManager.onProgress = (item, loaded, total) => {
+  if (loaded === total) {
+    saveLastTotalLoaded = total
+    hasFinishLoading.value = true
+  }
+  progress.value = Math.round(((loaded - saveLastTotalLoaded) / (total - saveLastTotalLoaded)) * 100 || 100, 2)
+}
 </script>
 
 <template>
+  <Transition
+    name="fade-overlay"
+    enter-active-class="opacity-1 transition-opacity duration-200"
+    leave-active-class="opacity-0 transition-opacity duration-200"
+  >
+    <div
+      v-show="!hasFinishLoading"
+      class="absolute bg-white t-0 l-0 w-full h-full z-20 flex justify-center items-center text-black"
+    >
+      <div class="w-200px">
+        <p>Loading 3D experience</p>
+        <SbLoading type="bar" size="normal" :model-value="progress" show-percentage color="primary" />
+      </div>
+    </div>
+  </Transition>
   <TresCanvas v-bind="gl" ref="context">
     <TresPerspectiveCamera :args="[45, 1, 0.1, 1000]" :position="[5, 5, 5]" />
     <Suspense>
